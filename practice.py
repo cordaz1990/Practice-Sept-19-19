cc6240ebe5de76bb83807fafbfed281b125f520e
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 23:26:12 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,12 @@
-print("hello")
-print("world")
+#Keywords Args
+def printall(*args):
+    print(args)
+    
+>>> print(1,2.0, '3')
+(1, 2.0, '3')
+
+>>>print(1, 2.0, '3')
+(1, 2.0, '3')
+
+>>>print(1, 2.0, third = '3')
+#TypeError: printall() got an unexpected keyword argument third
